518e2979fde63d7f23dd0bd7d057f1224a565f25
Yurij <Yurij2015@users.noreply.github.com>
2016-11-25 01:19:12 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,3 @@
-// console.time("timer");
-// for (var y = 0; y < 50000; y++) {
-//     var numReserve = [];
-//     while (numReserve.length < 1000000) {
-//         var number = Math.random() * 1000000;
-//         numReserve[numReserve.length] = Math.round(number);
-//     }
-//     var numReservelength = numReserve.length;
-//     console.log(numReservelength);
-//     console.log(numReserve);
-//     break
-// }
-// console.timeEnd("timer");
-
 console.time("timer");
 var numReserve = [];
 while (numReserve.length < 1000000) {
