b19b03f7bb449f312dab75d1a645b6073d9e6d30
Yurij <nabor_2008@i.ua>
2016-11-25 01:17:04 +0200

    skript - 2 task - delete part of code (wrong)

--- test.js
+++ test.js
@@ -1,17 +1,3 @@
-// console.time("timer");
-// for (var y = 0; y < 50000; y++) {
-//     var numReserve = [];
-//     while (numReserve.length < 1000000) {
-//         var number = Math.random() * 1000000;
-//         numReserve[numReserve.length] = Math.round(number);
-//     }
-//     var numReservelength = numReserve.length;
-//     console.log(numReservelength);
-//     console.log(numReserve);
-//     break
-// }
-// console.timeEnd("timer");
-
 console.time("timer");
 var numReserve = [];
 while (numReserve.length < 1000000) {
@@ -19,7 +5,7 @@ while (numReserve.length < 1000000) {
     numReserve[numReserve.length] = Math.round(number);
 }
 var numReservelength = numReserve.length;
-console.log(numReservelength);
+console.log("Количество чисел -", numReservelength);
 console.log(numReserve);
 console.timeEnd("timer");
 
