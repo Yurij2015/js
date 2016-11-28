be4347f4faf107e8b746981eb4e680594d5e4745
Yurij <Yurij2015@users.noreply.github.com>
2016-11-28 20:40:41 +0200

    Add files via upload

---
+++ transport-task.js
@@ -0,0 +1,45 @@
+var matrixC = [
+    [1, 3, 4, 2],
+    [4, 5, 8, 3],
+    [2, 3, 6, 7],
+];
+
+var matrixB = [40, 60, 80, 60];
+var matrixA = [60, 80, 100];
+
+
+// console.log(matrixC);
+// console.log(matrixA);
+// console.log(matrixB);
+// console.log(matrixC[0]);
+
+// document.write("<table>");
+// document.write("<tr>");
+// document.write("<td>");
+// document.write(matrixB[0]);
+// document.write("</td>");
+// document.write("<td>");
+// document.write(matrixB[1]);
+// document.write("</td>");
+// document.write("<td>");
+// document.write(matrixB[2]);
+// document.write("</td>");
+// document.write("<td>");
+// document.write(matrixB[3]);
+// document.write("</td>");
+// document.write("</tr>");
+
+document.write("<br>");
+
+document.write(" |" + matrixB);
+document.write("<br>");
+document.write(matrixA[0] + " |");
+document.write(matrixC[0]);
+document.write("<br>");
+document.write(matrixA[1] + " |");
+document.write(matrixC[1]);
+document.write("<br>");
+document.write(matrixA[2]+ "|");
+document.write(matrixC[2]);
+document.write("<br>");
+// document.write(matrixA);
